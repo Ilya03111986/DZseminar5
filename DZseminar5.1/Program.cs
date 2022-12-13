f819961d7c6c626a499389e69c9c1fdf2951c65a
Ilya Perederey <ilya_soldat@mail.ru>
2022-12-13 22:36:24 +0300

    DZ5.1

---
+++ DZseminar5.1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+int[] myArray = new int[4];
+Random rand = new Random();
+Console.Write ("[");
+int res = 0;
+    for (int i = 0; i < myArray.Length; i++)
+{   myArray[i] = rand.Next(999);
+        if (myArray[i] % 2 == 0) res++;
+    
+        Console.Write( myArray [i] +(i < myArray.Length -1? "," : ""));
+}           
+Console.Write("]"+"->"+ res);
+
